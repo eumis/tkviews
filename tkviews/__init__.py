0096b7584944e4b4ed3e9ddcc68cfdf7fe37737a
eumis <misievich@gmail.com>
2018-03-16 13:16:48 +0300

    refactoring

--- tkviews/__init__.py
+++ tkviews/__init__.py
@@ -1,8 +1,8 @@
 '''Package adapts pyviews for using with tkinter'''
 
+from pyviews.rendering.node import Code
 from .widgets import Root
 from .containers import Container, For, View, If
 from .styles import Style
 from .geometry import Row, Column
-from .code import Code
 from .canvas import Rectangle, Text, Image, Arc, Bitmap, Line, Oval, Polygon, Window

fd73641ba3af0c3117b6fb301ad5874bdbf29883
eumis <eugenmisievich@coherentsolutions.com>
2018-07-20 14:38:02 +0300

    added package imports

--- tkviews/__init__.py
+++ tkviews/__init__.py
@@ -1,8 +1,10 @@
 '''Package adapts pyviews for using with tkinter'''
 
 from pyviews.rendering.node import Code
+from pyviews.rendering.modifiers import set_global, import_global, inject_global
 from .node import Root
 from .containers import Container, For, View, If
 from .styles import Style
 from .geometry import Row, Column
 from .canvas import Rectangle, Text, Image, Arc, Bitmap, Line, Oval, Polygon, Window
+from .modifiers import bind, bind_all, call, set_attr, config, visible

47f6da515324e79958f64be1546e72025d30721e
eumis <eugenmisievich@coherentsolutions.com>
2018-10-08 16:50:27 +0300

    implemented canvas setup

---
+++ tkviews/canvas_setup.py
@@ -0,0 +1,59 @@
+'''
+Contains methods for node setups creation
+'''
+
+from pyviews import Node, NodeSetup, get_view_root
+from pyviews.core.ioc import SERVICES as deps
+from pyviews.core.observable import InheritedDict
+from pyviews.rendering.flow import apply_attributes, render_children
+from tkviews.core.canvas import CanvasNode
+
+def get_canvas_setup() -> NodeSetup:
+    '''Returns setup for canvas'''
+    node_setup = NodeSetup()
+    node_setup.render_steps = [
+        _setup_options_setter,
+        _setup_local_events,
+        apply_attributes,
+        _create_item,
+        _setup_config_setter,
+        _setup_event_binding
+    ]
+    return node_setup
+
+def _setup_options_setter(node: CanvasNode, **args):
+    node._attr_values = {}
+    node.setter = _set_option_value
+
+def _set_option_value(node: CanvasNode, key, value):
+    if hasattr(node, key):
+        setattr(node, key, value)
+    else:
+        node._attr_values[key] = value
+
+def _setup_local_events(node: CanvasNode, **args):
+    node._events = {}
+    node._bind_source = node.bind
+    node.bind = lambda event, command, n=node: _bind(n, event, command)
+
+def _bind(node: CanvasNode, event, command):
+    node._events[event] = command
+
+def _create_item(node: CanvasNode, **args):
+    node.create(node._attr_values)
+
+def _setup_config_setter(node: CanvasNode, **args):
+    node.setter = _set_config_value
+
+def _set_config_value(node: CanvasNode, key, value):
+    if hasattr(node, key):
+        setattr(node, key, value)
+    else:
+        node.config(**{key: value})
+
+def _setup_event_binding(node: CanvasNode, **args):
+    node.bind = node._bind_source
+    for event, command in node._events.items():
+        node.bind(event, command)
+    node._bind_source = None
+    node._events = None
---
+++ tkviews/core/canvas.py
@@ -0,0 +1,88 @@
+'''Wrappers for canvas elements'''
+
+from tkinter import Canvas
+from pyviews.core.xml import XmlNode
+from pyviews.core.observable import InheritedDict
+from pyviews.core import get_not_implemented_message
+from pyviews.core.node import Node
+
+class CanvasNode(Node):
+    '''Base class for wrappers'''
+    def __init__(self, master: Canvas, xml_node: XmlNode,
+                 node_globals: InheritedDict = None, node_styles: InheritedDict = None):
+        super().__init__(xml_node, node_globals=node_globals)
+        self._canvas = master
+        self._events = {}
+        self._item_id = None
+
+        self.place = None
+        self.node_styles = node_styles
+
+    @property
+    def item_id(self):
+        '''id returned from create method of canvas'''
+        return self._item_id
+
+    def create(self, **options):
+        '''Creates canvas element'''
+        self.item_id = self._create(**options)
+
+    def _create(self, **options):
+        raise NotImplementedError(get_not_implemented_message(self, '_create'))
+
+    def bind(self, event: str, command):
+        '''Binds element to event'''
+        self._canvas.tag_bind(self.item_id, '<' + event + '>', command)
+
+    def config(self, **options):
+        '''Calls itemconfig of canvas'''
+        self._canvas.itemconfig(self.item_id, **options)
+
+    def destroy(self):
+        '''Removes element from canvas'''
+        self._canvas.delete(self.item_id)
+
+class Rectangle(CanvasNode):
+    '''create_rectangle wrapper'''
+    def _create(self, **options):
+        return self._canvas.create_rectangle(*self._place, **options)
+
+class Text(CanvasNode):
+    '''create_text wrapper'''
+    def _create(self, **options):
+        return self._canvas.create_text(*self._place, **options)
+
+class Image(CanvasNode):
+    '''create_image wrapper'''
+    def _create(self, **options):
+        return self._canvas.create_image(*self._place, **options)
+
+class Arc(CanvasNode):
+    '''create_arc wrapper'''
+    def _create(self, **options):
+        return self._canvas.create_arc(*self._place, **options)
+
+class Bitmap(CanvasNode):
+    '''create_arc wrapper'''
+    def _create(self, **options):
+        return self._canvas.create_bitmap(*self._place, **options)
+
+class Line(CanvasNode):
+    '''create_line wrapper'''
+    def _create(self, **options):
+        return self._canvas.create_line(*self._place, **options)
+
+class Oval(CanvasNode):
+    '''create_oval wrapper'''
+    def _create(self, **options):
+        return self._canvas.create_oval(*self._place, **options)
+
+class Polygon(CanvasNode):
+    '''create_polygon wrapper'''
+    def _create(self, **options):
+        return self._canvas.create_polygon(*self._place, **options)
+
+class Window(CanvasNode):
+    '''create_window wrapper'''
+    def _create(self, **options):
+        return self._canvas.create_window(*self._place, **options)

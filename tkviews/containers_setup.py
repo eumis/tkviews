affe318bd6211af1b39b1c819bf835555d10fb86
eumis <eugenmisievich@coherentsolutions.com>
2018-10-08 13:13:10 +0300

    Implemented containers
    Logic from containers nodes is moved to setup

--- tkviews/containers_setup.py
+++ tkviews/containers_setup.py
@@ -4,31 +4,127 @@ Contains methods for node setups creation
 
 from pyviews import Node, NodeSetup, get_view_root
 from pyviews.core.ioc import SERVICES as deps
+from pyviews.core.observable import InheritedDict
 from pyviews.rendering.flow import apply_attributes, render_children
-from tkviews.core.containers import View
+from tkviews.core.containers import View, For, If
 
-def get_container_setup():
+def get_container_setup() -> NodeSetup:
     '''Returns setup for container'''
-    node_setup = NodeSetup(setter=setattr, get_child_args=_get_child_args)
+    node_setup = NodeSetup()
     node_setup.render_steps = [
         apply_attributes,
         render_children
     ]
+    node_setup.get_child_args = _get_child_args
     return node_setup
 
-def get_view_setup():
+def get_view_setup() -> NodeSetup:
     '''Returns setup for container'''
-    node_setup = NodeSetup(setter=setattr, get_child_args=_get_child_args)
+    node_setup = NodeSetup()
     node_setup.render_steps = [
         apply_attributes,
         _render_view_children
     ]
+    node_setup.get_child_args = _get_child_args
     return node_setup
 
-def _render_view_children(node: View, node_setup: NodeSetup, **args):
+def _render_view_children(node: View, node_setup: NodeSetup = None, **args):
     child_args = node_setup.get_child_init_args(node, **args)
     view_root = get_view_root(node.name)
     node.set_content(deps.render(view_root, **child_args))
+
+def get_for_setup() -> NodeSetup:
+    '''Returns setup for For node'''
+    node_setup = NodeSetup()
+    node_setup.render_steps = [
+        apply_attributes,
+        _subscribe_to_items_change,
+        _render_for
+    ]
+    node_setup.get_child_args = _get_child_args
+    return node_setup
+
+def _subscribe_to_items_change(node: For, node_setup: NodeSetup, **args):
+    node.items_observable.callback = lambda n, v, o, ns=node_setup: _on_items_changed(n, ns)
+
+def _on_items_changed(node: For, node_setup: NodeSetup):
+    _destroy_overflow(node)
+    _update_existing(node)
+    _create_not_existing(node, node_setup)
+
+def _destroy_overflow(node: For):
+    try:
+        items_count = len(node.items)
+        children_count = len(node.xml_node.children) * items_count
+        overflow = node._children[children_count:]
+        for child in overflow:
+            child.destroy()
+        node._children = node._children[:children_count]
+    except IndexError:
+        pass
+
+def _update_existing(node: For):
+    item_children_count = len(node.xml_node.children)
+    try:
+        for index, item in enumerate(node.items):
+            start = index * item_children_count
+            end = (index + 1) * item_children_count
+            for child_index in range(start, end):
+                globs = node._children[child_index].globals
+                globs['item'] = item
+    except IndexError:
+        pass
+
+def _create_not_existing(node: For, node_setup: NodeSetup):
+    item_children_count = len(node.xml_node.children)
+    start = int(len(node._children) / item_children_count)
+    end = len(node.items)
+    items = [node.items[i] for i in range(start, end)]
+    _render_for_children(node, node_setup, items)
+
+def _render_for_children(node: For, node_setup: NodeSetup, items: list):
+    item_xml_nodes = node.xml_node.children
+    for index, item in enumerate(items):
+        for xml_node in item_xml_nodes:
+            child_args = _get_for_child_args(node_setup, node, index, item)
+            child = deps.render(xml_node, **child_args)
+            node.add_child(child)
+    _render_for_children(node, node_setup, node.items)
+
+def _get_for_child_args(node: For, node_setup: NodeSetup, index, item):
+    child_args = node_setup.get_render_args(node)
+    child_globals = InheritedDict(child_args['node_globals'])
+    child_globals['index'] = index
+    child_globals['item'] = item
+    child_args['node_globals'] = child_globals
+    return child_args
+
+def _render_for(node: For, node_setup: NodeSetup = None, **args):
+    node.destroy_children()
+ 
+def get_if_setup() -> NodeSetup:
+    '''Returns setup for For node'''
+    node_setup = NodeSetup()
+    node_setup.render_steps = [
+        apply_attributes,
+        _subscribe_to_condition_change,
+        _render_if
+    ]
+    node_setup.get_child_args = _get_child_args
+    return node_setup
+
+def _subscribe_to_condition_change(node: If, node_setup: NodeSetup = None, **args):
+    node.condition_observable.callback = lambda n, v, o, ns=NodeSetup: _on_condition_change(n, ns, v, o, **args)
+
+def _on_condition_change(node: If, node_setup: NodeSetup, val: bool, old: bool, **args):
+    if val == old:
+        return
+    node.destroy_children()
+    _render_if(node, node_setup, **args)
+
+def _render_if(node: If, node_setup: NodeSetup = None, **args):
+    if node.condition:
+        render_children(node, node_setup=node_setup, **args)
 
 def _get_child_args(node: Node):
     return {
--- tkviews/core/containers.py
+++ tkviews/core/containers.py
@@ -4,7 +4,6 @@ Containers don't represent any widget.
 '''
 
 from tkinter import Widget
-from pyviews.core.ioc import inject
 from pyviews.core.xml import XmlNode
 from pyviews.core.observable import InheritedDict, observable_property
 from pyviews.core.node import Node
@@ -15,15 +14,13 @@ class Container(Node):
                  node_globals: InheritedDict = None, node_styles: InheritedDict = None):
         super().__init__(xml_node, node_globals=node_globals)
         self.master = master
-        self.styles = node_styles
+        self.node_styles = node_styles
 
-class View(Node):
+class View(Container):
     '''Loads xml from anothre file'''
     def __init__(self, master: Widget, xml_node: XmlNode,
                  node_globals: InheritedDict = None, node_styles: InheritedDict = None):
-        super().__init__(xml_node, node_globals=node_globals)
-        self.master = master
-        self.node_styles = node_styles
+        super().__init__(master, xml_node, node_globals=node_globals, node_styles=node_styles)
 
     (name, name_observable) = observable_property('_name')
 
@@ -33,98 +30,18 @@ class View(Node):
 
 class For(Container):
     '''Renders children for every item in items collection'''
-    def __init__(self, master, xml_node: XmlNode, parent_context=None):
-        super().__init__(master, xml_node, parent_context)
-        self._items = []
-        self._rendered = False
-        self._child_count = 0
+    def __init__(self, master: Widget, xml_node: XmlNode,
+                 node_globals: InheritedDict = None, node_styles: InheritedDict = None):
+        super().__init__(master, xml_node, node_globals=node_globals, node_styles=node_styles)
+        self.items = []
 
-    @property
-    def items(self):
-        '''Source collection'''
-        return self._items
-
-    @items.setter
-    def items(self, val):
-        self._items = val
-        if self._rendered:
-            self._destroy_overflow()
-            self._update_existing()
-            self._create_not_existing()
-
-    def _destroy_overflow(self):
-        try:
-            items_count = len(self._items)
-            children_count = self._child_count * items_count
-            overflow = self._children[children_count:]
-            for child in overflow:
-                child.destroy()
-            self._children = self._children[:children_count]
-        except IndexError:
-            pass
-
-    def _update_existing(self):
-        try:
-            for index, item in enumerate(self._items):
-                start = index * self._child_count
-                end = (index + 1) * self._child_count
-                for child_index in range(start, end):
-                    globs = self._children[child_index].globals
-                    globs['item'] = item
-        except IndexError:
-            pass
-
-    def _create_not_existing(self):
-        start = int(len(self._children) / self._child_count)
-        end = len(self._items)
-        self._render_children([(i, self._items[i]) for i in range(start, end)])
-
-    @inject('render')
-    def _render_children(self, items, render=None):
-        nodes = self.xml_node.children
-        for index, item in items:
-            for xml_node in nodes:
-                args = self.get_render_args(xml_node, index, item)
-                self._children.append(render(xml_node, args))
-
-    def render_children(self):
-        self._rendered = True
-        self._child_count = len(self.xml_node.children)
-        self.destroy_children()
-        self._render_children([(i, item) for i, item in enumerate(self._items)])
-
-    def get_render_args(self, xml_node: XmlNode, index=None, item=None):
-        args = super().get_render_args(xml_node)
-        context = args['parent_context'].copy()
-        item_globals = InheritedDict(context['globals'])
-        item_globals['index'] = index
-        item_globals['item'] = item
-        context['globals'] = item_globals
-        args['parent_context'] = context
-        return args
+    (items, items_observable) = observable_property('_items')
 
 class If(Container):
     '''Renders children if condition is True'''
-    def __init__(self, master, xml_node: XmlNode, parent_context=None):
-        super().__init__(master, xml_node, parent_context)
-        self._condition = False
-        self._rendered = False
+    def __init__(self, master: Widget, xml_node: XmlNode,
+                 node_globals: InheritedDict = None, node_styles: InheritedDict = None):
+        super().__init__(master, xml_node, node_globals=node_globals, node_styles=node_styles)
+        self.condition = False
 
-    @property
-    def condition(self):
-        '''Condition of rendering'''
-        return self._condition
-
-    @condition.setter
-    def condition(self, value):
-        if self._condition == value:
-            return
-        self._condition = value
-        if self._rendered:
-            self.destroy_children()
-            self.render_children()
-
-    def render_children(self):
-        if self._condition:
-            super().render_children()
-        self._rendered = True
+    (condition, condition_observable) = observable_property('_condition')

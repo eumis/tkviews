9e7c96b7474d57f43ed8512af4d24f1894c10d76
eumis <eumisdev@proton.me>
2022-11-16 10:27:48 +0100

    aded pyproject

--- setup.py
+++ setup.py
@@ -1,54 +1,11 @@
 """pypi package setup"""
 
-import codecs
-import re
-from os.path import join as join_path, dirname
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 def setup_package():
     """Package setup"""
-    setup(
-        name='tkviews',
-        version=_get_version(),
-        description='Package for creating tkinter applications in declarative way.',
-        long_description=_get_long_description(),
-        long_description_content_type='text/markdown',
-        url='https://github.com/eumis/tkviews',
-        author='eumis(Eugen Misievich)',
-        author_email='eug.mis@protonmail.com',
-        license='MIT',
-        classifiers=[
-            #   2 - Pre-Alpha
-            #   3 - Alpha
-            #   4 - Beta
-            #   5 - Production/Stable
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Developers',
-            'Topic :: Software Development :: Libraries',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3.6'
-        ],
-        python_requires='>=3.6',
-        install_requires=['pyviews>=3.*.*,<4.*.*'],
-        keywords='binding tkinter tk tkviews pyviews python mvvm views',
-        packages=find_packages(exclude=['demo', 'sandbox', '*.tests']))
-
-
-_HERE = dirname(__file__)
-
-
-def _get_version() -> str:
-    with open(join_path(_HERE, "tkviews", "__init__.py")) as package:
-        pattern = re.compile(r"__version__ = '(.*)'")
-        match = pattern.search(package.read())
-        return match.group(1)
-
-
-def _get_long_description():
-    readme_path = join_path(_HERE, "README.md")
-    with codecs.open(readme_path, encoding="utf-8") as readme:
-        return "\n" + readme.read()
+    setup()
 
 
 if __name__ == '__main__':
